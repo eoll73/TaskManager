a7e8685bafb4e2814b1429e210d7f4b2dfedf59d
Victor Vayuta <vittorio073@mail.ru>
2022-09-28 23:46:08 +0300

    fix generating models

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :name, :description, :state, :expired_at, :type] do |n|
     "string#{n}"
+  end
+
+  sequence :email do |n|
+    "person#{n}@example.com"
   end
 end
